64b0d9056b5d41ab963e1b7fa139db774fe1e587
denisdamse <146414688+denisdamse@users.noreply.github.com>
2023-10-16 23:24:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Impartire;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int imp = Console.Read();
+        int deimp = Console.Read();
+        int rezultat = deimp / imp;
+        if(deimp%imp==0)
+        { Console.WriteLine(rezultat); }
+        else { Console.WriteLine("Impartirea nu poate fi efectuata"); }
+    }
+}
+
